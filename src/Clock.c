3cc4b27d97779466e504b664851090fe68d0372f
Paolo Mazzon <pdmazzon@gmail.com>
2019-12-23 20:36:01 -0800

    Removed redundant line of code

--- src/Clock.c
+++ src/Clock.c
@@ -60,7 +60,6 @@ uint64_t ns() {
 void jamSleep(uint64_t nsTime) {
 #if defined(__linux)
 	struct timespec t;
-	t.tv_sec = (time_t)(int)(nsTime / 1000000000);
 	t.tv_nsec = nsTime;
 	nanosleep(&t, NULL);
 #elif defined(_WIN32)
